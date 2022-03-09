ebb8f6b056feaf44fc187e038ca1523b931a02d8
GKunc <grzeg.kunc@gmail.com>
2022-03-09 11:28:16 +0100

    Setup tailwind

--- Code/front-end/src/App.js
+++ Code/front-end/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -6,13 +5,14 @@ class App extends React.Component {
   
   render() {
     return (
-      <div className="App">
-        <form action="http://127.0.0.1:5000/decision_model" enctype="multipart/form-data" target="dummyframe" method="POST">
-          <input type="file" name="file" />
-          <input type="submit"/>
-        </form>   
-        <iframe style={{display: "none"}} name="dummyframe" id="dummyframe"></iframe>
-      </div>
+        <div className="App">
+          <h1 class="text-3xl font-bold underline">Hello world!</h1>
+          <form action="http://127.0.0.1:5000/decision_model" enctype="multipart/form-data" target="dummyframe" method="POST">
+            <input type="file" name="file" />
+            <input type="submit"/>
+          </form>   
+          <iframe style={{display: "none"}} title="dummyframe" name="dummyframe" id="dummyframe"></iframe>
+        </div>
     );
   }
 }
