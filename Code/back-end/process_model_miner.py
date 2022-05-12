cda41d1ec66bbc510404f58d57ad1c7cecd98dcc
GKunc <grzeg.kunc@gmail.com>
2022-05-12 10:21:30 +0200

    Remove files

--- Code/back-end/process_model_miner.py
+++
@@ -1,7 +0,0 @@
-from pm4py.algo.discovery.inductive import algorithm as inductive_miner
-
-
-class ProcessModelMiner:
-    def apply(self, log):
-        net, _, _ = inductive_miner.apply(log)
-        return net
