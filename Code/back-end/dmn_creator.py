054274aa6d2e8d69c4d80aec75a793bb04ca420b
GKunc <grzeg.kunc@gmail.com>
2022-08-12 17:37:44 +0200

    not fully working but connected

--- Code/back-end/dmn_creator.py
+++ Code/back-end/dmn_creator.py
@@ -1,6 +1,7 @@
 import xml.etree.cElementTree as ET
 
 
+# add only 1 element!!
 class DMNCreator:
     def apply(self, decisions, cfds, attributes, decision_model):
         print("++++++++++++")
@@ -10,6 +11,9 @@ class DMNCreator:
         print(decision_model)
         print("++++++++++++")
 
+        width = 100
+        height = 40
+        added_elements = []
         root = ET.Element(
             "definitions", xmlns="https://www.omg.org/spec/DMN/20191111/MODEL/")
         root.attrib["xmlns:dmndi"] = "https://www.omg.org/spec/DMN/20191111/DMNDI/"
@@ -19,66 +23,82 @@ class DMNCreator:
         # create decision
         # add dependend features
         for decision, reqs in decisions:
+            added_elements.append(decision)
             decision_element = ET.SubElement(
                 root, "decision", id=f"{decision}", name=f"{decision}")
             for req in reqs:
                 req_element = ET.SubElement(
                     decision_element, "informationRequirement", id=f"{req}_req")
-                ET.SubElement(req_element, "requiredInput", href=f"{req}")
+                ET.SubElement(req_element, "requiredInput", href=f"#{req}")
 
         for cfd, reqs in cfds:
+            added_elements.append(cfd)
             decision_element = ET.SubElement(
                 root, "decision", id=f"{cfd}", name=f"{cfd}")
             for req in reqs:
                 req_element_cfd = ET.SubElement(
                     decision_element, "informationRequirement", id=f"{req}_req")
-                ET.SubElement(req_element_cfd, "requiredInput", href=f"{req}")
-
-        decision = ET.SubElement(
-            root, "decision", id="Decision", name="Decision")
-        req = ET.SubElement(decision, "informationRequirement", id="Info1")
-        ET.SubElement(req, "requiredInput", href="Input1")
-
-        req2 = ET.SubElement(decision, "informationRequirement", id="Info2")
-        ET.SubElement(req2, "requiredInput", href="Input2")
+                ET.SubElement(req_element_cfd, "requiredInput", href=f"#{req}")
 
         # add all attributes
         for attribute in attributes:
+            added_elements.append(attribute)
             ET.SubElement(root, "inputData",
                           id=f'{attribute.replace(" ", "")}', name=f'{attribute}')
 
-        ET.SubElement(root, "inputData", id="Input1", name="Input 1 XD")
-        ET.SubElement(root, "inputData", id="Input2", name="Input 2")
-
         dmnDI = ET.SubElement(root, "dmndi:DMNDI")
         diagram = ET.SubElement(dmnDI, "dmndi:DMNDiagram", id="Diagram")
-        shape1 = ET.SubElement(diagram, "dmndi:DMNShape",
-                               dmnElementRef="Decision")
-        ET.SubElement(shape1, "dc:Bounds", height="80",
-                      width="180", x="270", y="260")
 
-        shape2 = ET.SubElement(diagram, "dmndi:DMNShape",
-                               dmnElementRef="Input1")
-        ET.SubElement(shape2, "dc:Bounds", height="45",
-                      width="125", x="407", y="118")
+        elements_with_coords = {}
 
-        shape3 = ET.SubElement(diagram, "dmndi:DMNShape",
-                               dmnElementRef="Input2")
-        ET.SubElement(shape3, "dc:Bounds", height="45",
-                      width="125", x="158", y="119")
+        x = 170
+        y = 60
+        for attribute in attributes:
+            x += 150
+            shape = ET.SubElement(diagram, "dmndi:DMNShape",
+                                  dmnElementRef=f"{attribute}")
 
-        edge1 = ET.SubElement(diagram, "dmndi:DMNEdge",
-                              dmnElementRef="Info1")
-        ET.SubElement(edge1, "di:waypoint", x="221", y="164")
-        ET.SubElement(edge1, "di:waypoint", x="330", y="240")
-        ET.SubElement(edge1, "di:waypoint", x="330", y="260")
+            ET.SubElement(shape, "dc:Bounds", height=f"{height}",
+                          width=f"{width}", x=f"{x}", y=f"{y}")
+            elements_with_coords[f"{attribute}"] = [x, y]
+        x = 170
+        y = 260
+        for decision, reqs in decisions:
+            x += 150
+            shape = ET.SubElement(diagram, "dmndi:DMNShape",
+                                  dmnElementRef=f"{decision}")
 
-        edge2 = ET.SubElement(diagram, "dmndi:DMNEdge",
-                              dmnElementRef="Info2")
-        ET.SubElement(edge2, "di:waypoint", x="470", y="163")
-        ET.SubElement(edge2, "di:waypoint", x="390", y="240")
-        ET.SubElement(edge2, "di:waypoint", x="390", y="260")
+            ET.SubElement(shape, "dc:Bounds", height=f"{height}",
+                          width=f"{width}", x=f"{x}", y=f"{y}")
+            elements_with_coords[f"{decision}"] = [x, y]
 
+        x = 170
+        y = 460
+        for cfd, reqs in cfds:
+            x += 150
+            shape = ET.SubElement(diagram, "dmndi:DMNShape",
+                                  dmnElementRef=f"{cfd}")
+
+            ET.SubElement(shape, "dc:Bounds", height=f"{height}",
+                          width=f"{width}", x=f"{x}", y=f"{y}")
+            elements_with_coords[f"{cfd}"] = [x, y]
+
+        # place reslationships here
+        for el1, el2 in decision_model:
+            x1, y1 = elements_with_coords[el1]
+            x2, y2 = elements_with_coords[el2]
+            if "p_" in el1:
+                edge = ET.SubElement(diagram, "dmndi:DMNEdge",
+                                     dmnElementRef=f"{el1}")
+            else:
+                edge = ET.SubElement(diagram, "dmndi:DMNEdge",
+                                     dmnElementRef=f"{el1}_req")
+            ET.SubElement(edge, "di:waypoint",
+                          x=f"{x1 + width/2}", y=f"{y1 + height}")
+            ET.SubElement(edge, "di:waypoint",
+                          x=f"{x2 + width/2}", y=f"{y2}")
+
+        print(elements_with_coords)
         tree = ET.ElementTree(root)
         tree.write("./uploads/result.dmn")
         return
